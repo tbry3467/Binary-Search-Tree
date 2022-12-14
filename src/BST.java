a857fe0e4ec4ca29ecf30fa37882f26a300b9eaa
Thomas Bryant <tbryant062@gmail.com>
2022-12-13 21:58:48 -0600

    Insert Methods Implemented
    Overloaded the insert method, if root is empty then the insert is now the root, otherwise recursively call insert till it finds an empty spot in the proper format

--- src/BST.java
+++ src/BST.java
@@ -19,13 +19,43 @@ public class BST {
     public BST() {
         root = null;
     }
+    public BST(int inData){
+        Node n = new Node(inData);
+        root = n;
+    }
     public int Search(int value) {
         return -1;
     }
     public int Delete(int value) {
         return -1;
     }
-    public int Insert(int value) {
-        return 1;
+    public void insert(int data) {
+        // if first item to be inserted, it becomes the root
+        if (root == null) {
+            Node n = new Node(data);
+            root = n;
+        }
+        insert(data, root);
+    }
+    public void insert(int data, Node n) {
+        // recursively calls itself till it finds an empty node in proper place
+        if (data > n.data){
+            if (n.left == null){
+                Node smaller = new Node(data, n);
+                n.left = smaller;
+            }
+            else{
+                insert(data, n.left);
+            }
+        }
+        else if (data < n.data){
+            if (n.right == null){
+                Node larger = new Node(data, n);
+                n.right = larger;
+            }
+            else{
+                insert(data, n.right);
+            }
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
         BST tree = new BST();
-        tree.Insert(3);
+        tree.insert(3);
+        tree.insert(2);
     }
 }
